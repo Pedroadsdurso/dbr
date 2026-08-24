366d931c3e008adf8cecd026f56c010aeb0c90af
Pedroadsdurso <145942104+Pedroadsdurso@users.noreply.github.com>
2026-08-24 15:23:16 -0300

    Add files via upload

---
+++ js/_buildManifest.js
@@ -0,0 +1,32 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/immutable/chunks/0kd2h-8vpnen_.js"
+  ],
+  "/_error": [
+    "static/immutable/chunks/00cltc70vgccm.js"
+  ],
+  "/inicio": [
+    "static/immutable/chunks/1sdr69g0ea4pf.js"
+  ],
+  "/privacidade": [
+    "static/immutable/chunks/0jm0_s3yz2s71.js"
+  ],
+  "/termos": [
+    "static/immutable/chunks/1f1p7ekt3qf85.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/go",
+    "/api/session-token",
+    "/inicio",
+    "/privacidade",
+    "/termos"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
